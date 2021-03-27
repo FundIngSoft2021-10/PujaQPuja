e0751523a3058eb511f834148cf0c123b57ca0fc
Juan Pablo Ortiz Rubio <45014827+lomitofrito@users.noreply.github.com>
2021-03-27 00:55:12 -0500

    Agregando imports necesarios Firebase

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:puja_que_puja/main.dart';
+import '../lib/screens/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
