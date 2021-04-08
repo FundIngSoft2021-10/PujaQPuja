d888c82c196f532383fe1419021a867561f85b86
Juan Pablo Ortiz Rubio <45014827+lomitofrito@users.noreply.github.com>
2021-04-08 13:03:12 -0500

    Cambios git ignore

--- src/main/java/pujaQpuja/InicioController.java
+++ src/main/java/pujaQpuja/InicioController.java
@@ -2,10 +2,17 @@ package pujaQpuja;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 
 public class InicioController implements Initializable {
+
+    @FXML
+    private Button botonRegistrarse;
+    @FXML
+    private Button botonIngresar;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
