3b6143655468b4d17f86e73fcf700c0591d1f124
Kenneth Leonel <kdavidleonelt@javeriana.edu.co>
2021-04-13 17:51:16 -0500

    Pantalla Inicio (Kenneth)
    se realizo las acciones de Inicio para que cambie a las pantallas de ingreso o de registro

--- src/main/java/pujaQpuja/controller/pantallas/InicioController.java
+++ src/main/java/pujaQpuja/controller/pantallas/InicioController.java
@@ -5,13 +5,18 @@
  */
 package pujaQpuja.controller.pantallas;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 /**
  * FXML Controller class
  *
@@ -33,12 +38,23 @@ public class InicioController implements Initializable {
     }
 
     @FXML
-    void ingresar(ActionEvent event) {
+    void ingresar(ActionEvent event) throws IOException {
+        Parent pantallaIngresoParent = FXMLLoader.load(getClass().getResource("/view/" +"Ingresar.fxml"));
+        Scene pantallaIngresofxml = new Scene(pantallaIngresoParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(pantallaIngresofxml);
+        window.show();
 
+        
     }
 
     @FXML
-    void registrar(ActionEvent event) {
+    void registrar(ActionEvent event) throws IOException {
+        Parent pantallaRegistroParent = FXMLLoader.load(getClass().getResource("/view/" +"PantallaRegistro.fxml"));
+        Scene pantallaRegistroScene = new Scene(pantallaRegistroParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(pantallaRegistroScene);
+        window.show();
 
     }
     
