3faab06e052ad69734173b6f0f5aed3c9f1dc435
Cristian Da Camara <cristianj.dacamaras@javeriana.edu.co>
2021-04-13 18:01:25 -0500

    Creacion de pantalla exito registro
    Se creo pantalla de exito registro, controlador y verificacion de campos de registro

--- src/main/java/pujaQpuja/controller/pantallas/PantallaRegistroController.java
+++ src/main/java/pujaQpuja/controller/pantallas/PantallaRegistroController.java
@@ -15,12 +15,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import pujaQpuja.model.Usuario;
+
 /**
  * FXML Controller class
  *
@@ -28,7 +28,6 @@ import pujaQpuja.model.Usuario;
  */
 
 public class PantallaRegistroController implements Initializable {
-
 
     @FXML
     private TextField campoNombreApellido;
@@ -46,31 +45,60 @@ public class PantallaRegistroController implements Initializable {
     private CheckBox checkAceptarTerminos;
     @FXML
     private Button botonRegistrarse;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //Usuario usuario = new Usuario();
-    }    
-    
+        // Usuario usuario = new Usuario();
+    }
+
+    static boolean isValid(String email) {
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        return email.matches(regex);
+    }
+
+    static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
     @FXML
     private void registrarse(ActionEvent event) throws IOException {
         Usuario usuario = new Usuario();
-        if(campoPassword.getText().equals(campoConfirmarPassword.getText()) && checkAceptarTerminos.isSelected())
-        {
+
+        if (campoPassword.getText().equals(campoConfirmarPassword.getText()) && checkAceptarTerminos.isSelected()
+                && isNumeric(campoTelefono.getText()) && isValid(campoCorreo.getText())) {
             usuario.setApellidos(campoNombreApellido.getText());
             usuario.setCorreo(campoCorreo.getText());
             usuario.setDireccion(campoDireccion.getText());
             usuario.setTelefono(campoTelefono.getText());
             usuario.setPassword(campoPassword.getText());
-        }else
-        {
-           Parent pantallaErrorParent = FXMLLoader.load(getClass().getResource("/view/" +"PantallaErrorRegistro.fxml"));
-           Scene errorRegistroScene = new Scene(pantallaErrorParent);
-           Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-           window.setScene(errorRegistroScene);
-           window.show();
+            System.out.println("holiwi");
+
+            /*
+             * Parent pantallaErrorParent = FXMLLoader.load(getClass().getResource("/view/"
+             * +"PantallaExitoRegistro.fxml")); Scene errorRegistroScene = new
+             * Scene(pantallaErrorParent); Stage window =
+             * (Stage)((Node)event.getSource()).getScene().getWindow();
+             * window.setScene(errorRegistroScene); window.show();
+             */
+
+        } else {
+            Parent pantallaErrorParent = FXMLLoader
+                    .load(getClass().getResource("/view/" + "PantallaErrorRegistro.fxml"));
+            Scene errorRegistroScene = new Scene(pantallaErrorParent);
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(errorRegistroScene);
+            window.show();
         }
     }
 
