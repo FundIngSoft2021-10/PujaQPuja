a9068ef02252134b16602882fa7ad5d7660d805c
Cristian Da Camara <cristianj.dacamaras@javeriana.edu.co>
2021-04-13 19:13:06 -0500

    Cambio pantalla editar perfil

--- src/main/java/pujaQpuja/controller/pantallas/EditarPerfilController.java
+++ src/main/java/pujaQpuja/controller/pantallas/EditarPerfilController.java
@@ -5,6 +5,7 @@
  */
 package pujaQpuja.controller.pantallas;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,6 +18,11 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import pujaQpuja.Utiles;
 import pujaQpuja.model.Usuario;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 /**
  * FXML Controller class
  *
@@ -50,31 +56,42 @@ public class EditarPerfilController implements Initializable {
     }    
 
     @FXML
-    void cancelar(ActionEvent event) {
-
+    void cancelar(ActionEvent event) throws IOException {
+        /*Parent pantallaErrorParent = FXMLLoader.load(getClass().getResource("/view/" +"PantallaHome.fxml"));
+        Scene errorRegistroScene = new Scene(pantallaErrorParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(errorRegistroScene);
+        window.show();*/
     }
 
     @FXML
     void guardar(ActionEvent event) {
         Usuario usuario = new Usuario();
+
         if(!campoNombreApellido.getText().isBlank()){
-            
+            usuario.setApellidos(campoNombreApellido.getText());
         }
         if(!campoCorreo.getText().isBlank() && Utiles.isValid(campoCorreo.getText())){
-            
+            usuario.setCorreo(campoCorreo.getText());
         }
         if(!campoPassword.getText().isBlank()){
-            
+            usuario.setPassword(campoPassword.getText());
         }
         if(!campoDocumento.getText().isBlank()){
-            
+            usuario.setDocumento(campoDocumento.getText());
         }
         if(!campoTelefono.getText().isBlank() && Utiles.isNumeric(campoTelefono.getText())){
-            
+            usuario.setTelefono(campoTelefono.getText());
         }
         if(!campoDireccion.getText().isBlank()){
-            
+            usuario.setDireccion(campoDireccion.getText());
         }
+        
+        Parent pantallaErrorParent = FXMLLoader.load(getClass().getResource("/view/" +"PantallaHome.fxml"));
+        Scene errorRegistroScene = new Scene(pantallaErrorParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(errorRegistroScene);
+        window.show();
     }
     
 }
